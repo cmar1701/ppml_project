062df67e037c2dbab45de0ad58e482edd6d1ce4d
CarterMarkegard <43353006+CarterMarkegard@users.noreply.github.com>
2024-05-28 21:41:09 -0700

    Added CUDA to the training process

--- eda.py
+++ eda.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 
 def main():
-    df = pd.read_csv("HAM10000_metadata.csv")
-    df.describe()
-    df.shape
-    df.head()
+    df = pd.read_csv("data/HAM10000_metadata.csv")
+    print(f"Describe: \n{df.describe()}")
+    print(f"Shape: \n{df.shape}")
+    print(f"Head: \n{df.head()}")
 
 if __name__ == "__main__":
     main()
